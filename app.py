d49208d95586da35c86428b776f91c3fc9525f8c
Salman Shaikh <tony903212@gmail.com>
2020-12-28 14:40:28 +0530

    Polished app and stylings

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 from flask import Flask, render_template
+from datetime import datetime
 from flask_socketio import SocketIO, send, emit
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'AFPurmxhSC06jlHwDbZzBvFs'
 socketio = SocketIO(app)
 
-@app.route('/', defaults={'path': ''})
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
 @socketio.on('send')
-def message_recieved(msg):
-    emit('recieve', msg, broadcast=True)
+def message_recieved(data):
+    data['timestamp'] = datetime.now().strftime('%I:%M')
+    emit('recieve', data, broadcast=True)
 
 
 if __name__ == '__main__':
--- static/js/main.js
+++ static/js/main.js
@@ -1,27 +1,65 @@
 const sendBox = document.querySelector('.send').children
 const chatBox = document.querySelector('.history')
+const darkBtn = document.querySelector('.header img')
 var socket = io()
-
 clientId = localStorage.getItem('clientId') 
+clientName = localStorage.getItem('clientName')
+
 if (!clientId) {
     clientId = Math.floor(Math.random() * 10000000000000001)
     localStorage.setItem('clientId', clientId)
     console.log(clientId)
 }
 
+if (!clientName) {
+    clientName = prompt('Enter your name:')
+    while (clientName == ''){
+        clientName = prompt('Enter your name(Don\'t try to be oversmart enter your proper name):')
+    }
+    localStorage.setItem('clientName', clientName)
+}
+
+function darkMode() {
+    status = document.querySelector("body").classList.toggle("dark")
+    localStorage.setItem("darkmode", status)
+    if (status == "true") {
+        darkBtn.src = "static/images/sun-icon.webp"
+        return status
+    }
+    darkBtn.src = "static/images/moon-icon.webp"
+    return status
+}
 socket.on('recieve', (data) => {
     var msgBubble = document.createElement('div')
-    if (clientId == data.clientId){
+    msgBubble.classList.add('msgBubble')
+    msgBubble.innerHTML = data.message
+    msgBubble.innerHTML += data.timestamp.small()
+    if (clientId == data.clientDetails.clientId){
         msgBubble.classList.add('msgSent')
     }
     else {
+        var msgName = document.createElement('div')
         msgBubble.classList.add('msgRecieved')
+        msgName.innerHTML = data.clientDetails.clientName + ':'
+        msgName.classList.add('name')
+        msgBubble.prepend(msgName)
     }
-    msgBubble.innerHTML = data.message
     chatBox.append(msgBubble)
     console.log(data)
+    window.scrollBy(0, 100)
 })
 
 sendBox[1].addEventListener('click', () => {
-    socket.emit('send', {'message': sendBox[0].value, 'clientId': clientId})
+    hours = new Date().toLocaleTimeString()
+    if (sendBox[0].value != ''){
+        data = {
+            'clientDetails': {
+                'clientId': clientId,
+                'clientName': clientName
+            },
+            'message': sendBox[0].value
+        }
+        socket.emit('send', data)
+        sendBox[0].value = ''
+    }
 })
